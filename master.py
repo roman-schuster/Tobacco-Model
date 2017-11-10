cc14dc2f32b2c24befa848f63405937764e01d60
Roman Schuster <schuster@sas.upenn.edu>
2017-11-10 14:54:26 -0500

    Update master.py

--- master.py
+++ master.py
@@ -1,5 +1,7 @@
 import xlwings as xw
 
+## Variables ##
+# XL Wing Variables #
 workbook = xw.Book(r'C:\path\to\file.xlsx')
 
 worksheet_turbos = workbook.sheets['Turbo Bonds']
@@ -7,16 +9,21 @@ worksheet_serials = workbook.sheets['Serial Bonds']
 worksheet_dsrf = workbook.sheets['DSRF']
 worksheet_rev = worksheet.sheets['Pledged Revenue']
 
-num_turbos = worksheet_turbos.range('B1').value
-num_serials = worksheet_serials.range('B1').value
-
+# Major Variables #
 turbo_bonds = []
 serial_bonds = []
 pledged_revs = {}
+
 dsrf_max = worksheet_dsrf.range('B3').value
 dsrf_current = worksheet_dsrf.range('B4').value
-current_year = 2017
+dsrfIsFull = True if (dsrf_max == dsrf_current) else False
 
+current_year = 2017
+end_year = 2099
+
+# Excel-Python Helper Variables #
+num_turbos = worksheet_turbos.range('B1').value
+num_serials = worksheet_serials.range('B1').value
 
 turbo_ints_to_cols = {0:'B', 1:'H', 2:'N', 3:'T', 4:'Z', 5:'AF',
     6:'AL', 7:'AR', 8:'AX', 9:'BD', 10:'BJ',
@@ -28,6 +35,8 @@ serial_ints_to_cols = {0:'B', 1:'G', 2:'L', 3:'Q', 4:'V', 5:'AA',
     11:'BE', 12:'BJ', 13:'BO', 14:'BT', 15:'BY',
     16:'CD', 17:'CI', 18:'CN', 19:'CS', 20:'CX'}
 
+## Initialization ##
+# Initializing Turbo Bonds #
 for i in range(num_turbos):
     cusip = worksheet_turbos.range(turbo_ints_to_cols[i] + cstr(3)).value
     maturity = worksheet_turbos.range(turbo_ints_to_cols[i] + cstr(4)).value
@@ -37,9 +46,11 @@ for i in range(num_turbos):
     
     turbo_bonds.append(Turbo_Bond(cusip, maturity, coupon, prop_of_revs, amt_outstanding))
 
+# Initializing Pledged Revenues #
 for i in range(5, 88):
     pledged_revs[current_year + (i - 5)] = worksheet_rev.range('B' + cstr(i)).value
     
+# Initializing Serial Bonds #    
 for i in range(num_serials):
     cusip = worksheet_serials.range(serial_ints_to_cols[i] + cstr(3)).value
     maturity = worksheet_serials.range(serial_ints_to_cols[i] + cstr(4)).value
@@ -48,4 +59,12 @@ for i in range(num_serials):
     
     serial_bonds.append(Serial_Bond(cusip, maturity, coupon, amt_outstanding))
     
-  
+## Main ##   
+for year in range(Current_year, end_year):
+    
+    # Minor Variables #
+    total_payments = 0
+    december_interest_payments = 0
+    available_revs = pledged_revs[year]
+    
+    
